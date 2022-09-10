e4dd23dd103e32c0d5975a4f7d25da644b76eb34
Helin Faidhan <helinkurd1997@gmail.com>
2022-09-10 21:38:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let a = 2;
 
-// Write your code here
+console.log(a *= 31);
+
+var random = Math.random();
+
+console.log(random);
+
+var num3 = 1999;
+
+
+console.log(num3 % 5);
+
+
+console.log(Math.max(1, 3, 2,20,19,18));
